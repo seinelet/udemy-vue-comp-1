36974a782500c725caa3e40b3b2d2f8985303ee0
AtsushiTashiro <atsushi.tashiro@phoneappli.net>
2019-09-10 16:59:04 +0900

    Section 10 Finish

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div.row
+    app-quote(v-for="(quote, index) in quotes" @click.native="deleteQuote(index)") {{ quote }}
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
