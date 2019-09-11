f9a879608548901f9043f336f96f3b0a2b9f561d
AtsushiTashiro <atsushi.tashiro@phoneappli.net>
2019-09-11 15:14:54 +0900

    Section 14 Start

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,16 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filters & Mixins</h1>
-        <p>{{ text | toUppercase | toLowercase }}</p>
-        <hr />
-        <div>
-          <button @click="fruits.push('Berries')">Add New Item</button>
-        </div>
-        <input v-model="filterText" />
-        <ul>
-          <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
-        </ul>
-        <hr />
-        <app-list></app-list>
+        <h1>Animations</h1>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import List from "./List.vue";
-import { fruitMixin } from "./fruitMixin";
-
 export default {
-  mixins: [fruitMixin],
   data() {
-    return {
-      text: "Hello there!"
-    };
-  },
-  filters: {
-    toUppercase(value) {
-      return value.toUpperCase();
-    }
-  },
-  components: {
-    appList: List
+    return {};
   }
 };
 </script>
