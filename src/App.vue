6962b6218dab29c883a499c8dd57490cd5515446
AtsushiTashiro <atsushi.tashiro@phoneappli.net>
2019-09-11 17:02:19 +0900

    201 Finish

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,35 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr />
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br />
+        <br />
         <button class="btn btn-primary" @click="show = !show">
           Show Alert
         </button>
         <br /><br />
-        <transition name="fade">
+        <transition :name="alertAnimation">
           <div class="alert alert-info" v-show="show">This is some Info</div>
         </transition>
-        <transition name="slide" type="animation">
+        <transition name="slide" type="animation" appear>
           <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+        <transition
+          enter-active-class="animated bounce"
+          leave-active-class="animated shake"
+        >
+          <div class="alert alert-info" v-show="show">This is some Info</div>
+        </transition>
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">
+            This is some Info
+          </div>
+          <div class="alert alert-warning" v-else key="warning">
+            This is some Warning
+          </div>
         </transition>
       </div>
     </div>
@@ -23,7 +43,8 @@
 export default {
   data() {
     return {
-      show: false
+      show: true,
+      alertAnimation: "fade"
     };
   }
 };
@@ -55,7 +76,7 @@ export default {
 }
 .slide-leave-active {
   animation: slide-out 1s ease-out forwards;
-  transition: opacity 0.5s;
+  transition: opacity 1s;
   opacity: 0;
 }
 
