818613afd18b182615ca4d0e3d08e46290aa51f9
AtsushiTashiro <atsushi.tashiro@phoneappli.net>
2019-09-12 17:14:45 +0900

    250 Finish

--- src/routes.js
+++ src/routes.js
@@ -25,5 +25,7 @@ export const routes = [
       { path: ":id", component: UserDetail },
       { path: ":id/edit", component: UserEdit, name: "userEdit" }
     ]
-  }
+  },
+  { path: "/redirect-me", redirect: { name: "home" } },
+  { path: "*", redirect: "/" }
 ];
