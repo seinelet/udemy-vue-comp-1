6380c1ede08a582dc6d52d8b2be617ecc0539d72
AtsushiTashiro <atsushi.tashiro@phoneappli.net>
2019-09-12 18:29:58 +0900

    Section 16 Finish

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,22 @@
-import User from "./components/user/User.vue";
-import UserStart from "./components/user/UserStart.vue";
-import UserDetail from "./components/user/UserDetail.vue";
-import UserEdit from "./components/user/UserEdit.vue";
+// import User from "./components/user/User.vue";
+// import UserStart from "./components/user/UserStart.vue";
+// import UserDetail from "./components/user/UserDetail.vue";
+// import UserEdit from "./components/user/UserEdit.vue";
 import Home from "./components/Home.vue";
 import Header from "./components/Header.vue";
+
+const User = () =>
+  import(/* webpackChunkName: "group-user" */ "./components/user/User.vue");
+const UserStart = () =>
+  import(
+    /* webpackChunkName: "group-user" */ "./components/user/UserStart.vue"
+  );
+const UserDetail = () =>
+  import(
+    /* webpackChunkName: "group-user" */ "./components/user/UserDetail.vue"
+  );
+const UserEdit = () =>
+  import(/* webpackChunkName: "group-user" */ "./components/user/UserEdit.vue");
 
 export const routes = [
   {
@@ -22,7 +35,14 @@ export const routes = [
     },
     children: [
       { path: "", component: UserStart },
-      { path: ":id", component: UserDetail },
+      {
+        path: ":id",
+        component: UserDetail,
+        beforeEnter: (to, from, next) => {
+          console.log("inside route setup");
+          next();
+        }
+      },
       { path: ":id/edit", component: UserEdit, name: "userEdit" }
     ]
   },
