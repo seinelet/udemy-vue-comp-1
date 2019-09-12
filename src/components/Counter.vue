b8b44d000cae159183cc02b6ba15b76a98275758
atsushi tashiro <selenite.white@gmail.com>
2019-09-13 00:09:34 +0900

    indent change

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <button class="btn btn-primary" @click="increment">Increment</button>
+    <button
+      class="btn btn-primary"
+      style="margin-right: 5px;"
+      @click="increment"
+    >
+      Increment
+    </button>
     <button class="btn btn-primary" @click="decrement">Decrement</button>
   </div>
 </template>
