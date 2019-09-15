284fe011b1565386db09273d4e099ed56ada768a
atsushi tashiro <selenite.white@gmail.com>
2019-09-15 22:34:11 +0900

    Sectin 18 Finish

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,8 +19,12 @@
             >Funds: {{ funds | currency }}</strong
           >
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">End Day</a></li>
-            <li class="dropdown">
+            <li><a href="#" @click="endDay">End Day</a></li>
+            <li
+              class="dropdown"
+              :class="{ open: isDropdownOpen }"
+              @click="isDropdownOpen = !isDropdownOpen"
+            >
               <a
                 href="#"
                 class="dropdown-toggle"
@@ -31,8 +35,8 @@
                 >Save & Load <span class="caret"></span
               ></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Save Data</a></li>
-                <li><a href="#">Load Data</a></li>
+                <li><a href="#" @click="saveData">Save Data</a></li>
+                <li><a href="#" @click="loadData">Load Data</a></li>
               </ul>
             </li>
           </ul>
@@ -45,10 +49,37 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
+  data() {
+    return {
+      isDropdownOpen: false
+    };
+  },
   computed: {
     funds() {
       return this.$store.getters.funds;
+    }
+  },
+  methods: {
+    ...mapActions({
+      randomizeStocks: "randomizeStocks",
+      fetchData: "loadData"
+    }),
+    endDay() {
+      this.randomizeStocks();
+    },
+    saveData() {
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks
+      };
+      this.$http.put("data.json", data);
+    },
+    loadData() {
+      this.fetchData();
     }
   }
 };
